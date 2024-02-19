40f1808726d3db1a5e8cd2651cc06e99e49bc2b8
Toby Armond <armond.toby@gmail.com>
2024-02-19 15:44:21 +0000

    Initial commit to branch

--- feedtrac/login.php
+++ feedtrac/login.php
@@ -48,7 +48,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 <html>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+    <link rel="stylesheet" type="text/css" href="login.css">
 	<body>
 
 		<!-- Title -->
