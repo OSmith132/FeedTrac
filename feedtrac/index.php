8b82388c06244a1dcf4557db83911b67d7affd4d
Archie Baldry <33762310+archiebaldry@users.noreply.github.com>
2024-02-25 20:34:39 +0000

    Initial commit

--- feedtrac/index.php
+++ feedtrac/index.php
@@ -1,78 +1,114 @@
-
-<?php 
-session_start();
-
-	include("connection.php");
-	include("functions.php");
-
-	$user_data = check_login($con);
-?>
-
 <!DOCTYPE html>
-<html>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<html lang="en-gb">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<body>
-		
-		<!-- Title -->
-		<head>
-			<title>FeedTrac</title>
-		</head>
+    <title>FeedTrac</title>
 
-		<!-- Header -->
-		<h1>This is the index page</h1>
-		Hello, <?php echo $user_data['username']; ?> <br>
-		<a href="logout.php">Logout</a>
-		<br><br>
+    <link rel="icon" type="image/x-icon" href="assets/icon.png">
 
-		
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="stylesheets/main.css">
 
+    <script src="https://kit.fontawesome.com/7e1870387e.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="header">
+        <a class="logo" title="Homepage" href="index.php">
+            <img src="assets/icon.png" alt="FeedTrac Icon" height="32">
 
-	<!-- Change Dark/Light Modes -->
-	
-		<style>
-			body {
-				padding: 15px;
-				background-color: #353535;
-				color: white;
-				font-size: 15px;
-			}
-	
-			.dark-mode {
-				background-color: #353535;
-				color: white;
-			}
-	
-			.light-mode {
-				background-color: white;
-				color: black;
-			}
-		</style>
+            <h1>FeedTrac</h1>
+        </a>
 
-	<h3 id="DarkModetext">Dark Mode is ON</h3>
-    <button onclick="darkMode()">Dark Mode</button>
-    <button onclick="lightMode()">Light Mode</button>
+        <div class="header-right">
+            <button title="Light Mode">
+                <i class="fa-regular fa-lightbulb"></i>
+            </button>
 
-    <script>
-        function darkMode() {
-            let element = document.body;
-            let content = document.getElementById("DarkModetext");
-            element.className = "dark-mode";
-            content.innerText = "Dark Mode is ON";
-        }
-        function lightMode() {
-            let element = document.body;
-            let content = document.getElementById("DarkModetext");
-            element.className = "light-mode";
-            content.innerText = "Dark Mode is OFF";
-        }
-    </script>
+            <button title="Inbox">
+                <i class="fa-solid fa-inbox"></i>
+            </button>
 
-		
-	
-	
-		
-	</body>
-</hthml>
+            <a title="Profile" href="#">
+                <img class="avatar" src="assets/avatar.jpg" alt="User Avatar" height="32">
+            </a>
+        </div>
+    </div>
+
+    <div class="main">
+        <div class="list-header">
+            <input class="search-bar" type="text" placeholder="Filter existing Feedback...">
+
+            <button>New Feedback</button>
+        </div>
+
+        <table>
+            <tr>
+                <th>Status</th>
+                <th>Priority</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Tags</th>
+                <th>Engagement</th>
+            </tr>
+            <tr>
+                <td><button>Open</button></td>
+                <td><button>High</button></td>
+                <td><a href="feedback.php">Lights keep flickering during lecture</a></td>
+                <td><a href="#">Archie Baldry (26411141)</a></td>
+                <td>
+                    <button>CMP1010</button>
+                    <button>Lecture</button>
+                    <button>Room</button>
+                    <button>Lighting</button>
+                </td>
+                <td>+69 (12 comments)</td>
+            </tr>
+            <tr>
+                <td><button>Closed</button></td>
+                <td><button>Medium</button></td>
+                <td><a href="feedback.php">Example 2</a></td>
+                <td><a href="#">Archie Baldry (26411141)</a></td>
+                <td>
+                    <button>CMP1010</button>
+                    <button>Lecture</button>
+                    <button>Room</button>
+                    <button>Lighting</button>
+                </td>
+                <td>+69 (12 comments)</td>
+            </tr>
+            <tr>
+                <td><button>Closed</button></td>
+                <td><button>Low</button></td>
+                <td><a href="feedback.php">Example 3</a></td>
+                <td><a href="#">Archie Baldry (26411141)</a></td>
+                <td>
+                    <button>CMP1010</button>
+                    <button>Lecture</button>
+                    <button>Room</button>
+                    <button>Lighting</button>
+                </td>
+                <td>+69 (12 comments)</td>
+            </tr>
+        </table>
+
+        <div class="footer">
+            <p>© 2024 The FeedTrac Team</p>
+
+            <a href="#">Terms</a>
+
+            <a href="#">Privacy</a>
+
+            <a href="https://github.com/OSmith132/FeedTrac/">Source</a>
+
+            <a href="#">Contact</a>
+        </div>
+    </div>
+
+    <script src="scripts/main.js"></script>
+</body>
+</html>
