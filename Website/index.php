149e13b23ca3db7096710edac7c41017b71d24b9
Oliver Smith <osmith04@btinternet.com>
2024-05-01 20:42:17 +0100

    Finished sorting and filtering

--- Website/index.php
+++ Website/index.php
@@ -56,11 +56,8 @@ $Feedback_View = new FeedbackView($user_data['userID']);
         <div class="index-main">
             <div class="index-header">
 
-            
-
-            
                 <!-- Search-bar functionality -->
-                <form class="index-header" method="POST" action="index.php" id="index-search-form">
+                <form class="index-header" method="POST" action="index.php" id="index-search-form" .change() >
 
                     <!-- Filter Methods -->
                     <div class="index-toggle-box">
@@ -70,10 +67,8 @@ $Feedback_View = new FeedbackView($user_data['userID']);
 
                         <input class="search-bar" type="text" name="searchTerm" placeholder="Search for existing feedback">
 
-                        <button name="submit">Search</button>
+                        <!-- <button name="submit">Search</button> -->
                     </div>
-                
-
 
                     <!-- Filter Methods -->
                     <div class="index-toggle-box">
@@ -122,229 +117,156 @@ $Feedback_View = new FeedbackView($user_data['userID']);
                             <span id="timeframe-value">All Time</span>
                         </div>
 
-                        <!-- Submit -->
-                        <!-- <button type="submit">Apply Filters</button> -->
+                    </div>
 
-                    
-                </div>
+                    <!-- Sort Methods -->
+                    <div class="sort-box">
+                        <h2>Sort Options</h2>
+
+                            <!-- Sort by Rating Points -->
+                            <div class="index-sort-option">
+                                <label for="sort-type">Sort Type:</label>
+                                <select id="sort-type" name="sort-type">
+                                    <option value="relevance">Relevance</option>
+                                    <option value="date">Date</option>  
+                                    <option value="urgency">Urgency</option>
+                                    <option value="ratingPoints">Rating</option>
+                                </select>
+                            </div>
+
+
+                            <!-- Direction -->
+                            <div class="index-sort-option">
+                                <label for="Direction">Direction:</label>
+                                <select id="Direction" name="sort-direction">
+                                    <option value="asc">Ascending</option>
+                                    <option value="desc">Descending</option>
+                                </select>
+                            </div>
+                    </div>
+                </form>
 
             
 
-
-                <!-- Sort Methods -->
-                <div class="sort-box">
-                    <h2>Sort Options</h2>
-
-                        <!-- Sort by Rating Points -->
-                        <div class="index-sort-option">
-                            <label for="sort-type">Sort Type:</label>
-                            <select id="sort-type" name="sort-type">
-                                <option value="date">Date</option>
-                                <option value="urgency">Urgency</option>
-                                <option value="rating">Rating</option>
-                            </select>
-                        </div>
-
-
-                        <!-- Direction -->
-                        <div class="index-sort-option">
-                            <label for="Direction">Direction:</label>
-                            <select id="Direction" name="sort-direction">
-                                <option value="asc">Ascending</option>
-                                <option value="desc">Descending</option>
-                            </select>
-                        </div>
-
-                        <!-- Submit -->
-                        <!-- <button type="submit">Apply Sorting</button> -->
-
-
-                    
-                </div>
-
-
-            </form>
-
-
-                        
-
-                New Feedback Button
+                <!-- New Feedback Button -->
                 <button onclick="window.location.href = 'newFeedback.php'">New Feedback</button> 
+
             </div>
 
 
-
-
-
-
-            <!-- Get result from database to fill table -->
-            <?php
-
-
-                       
-                    
-
-                    // If search button is pressed, get search term and display results
-                    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['action'] == 'search') {
-
-                        $searchTerm = $_POST['searchTerm'];
-
-
-                        $feedbackRows = $Feedback_View->search_Feedback($searchTerm);
-
-
-                        // If no search term entered, display all feedback
-                        if (empty($searchTerm)) {
-                            $feedbackRows = $Feedback_View->get_all_feedback();
-                        }
-                        // If no results found, display message
-                        else if (empty($feedbackRows)) {
-                            echo "<i>No results found for '" . $searchTerm . "'</i>";
-                        }
-
-
-                        // Filter Options
-                        $resolved = isset($_POST['resolved']) ? 1 : 0;  
-                        $closed = isset($_POST['closed']) ? 1 : 0;  
-                        $urgency    = $_POST['urgency'];          // 0,1,2,3,4  =>  All, Low, Medium, High, Critical
-                        $timeframe  = $_POST['timeframe'];        // 0,1,2,3,4  =>  1 Hour, 1 Day, 1 Week, 1 Month, All Time
-
-                        // Sorting Options
-                        $sortType    = $_POST['sort-type'];   
-                        $sortDirection    = $_POST['sort-direction']; 
-                        
-
-
-                        // $_SESSION["resolved"]   = isset($_POST['resolved']);  // 0,1
-                        // $_SESSION["closed"]     = isset($_POST['closed']);    // 0,1
-                        // $_SESSION["urgency"]    = $_POST['urgency'];          // 0,1,2,3,4  =>  All, Low, Medium, High, Critical
-                        // $_SESSION["timeframe"]  = $_POST['timeframe'];        // 0,1,2,3,4  =>  1 Hour, 1 Day, 1 Week, 1 Month, All Time
-
-                        // // output all options
-                        // echo "Search Term: " . $searchTerm . "<br>";
-                        // echo "Resolved: " . $resolved . "<br>";
-                        // echo "Closed: " . $closed . "<br>";
-                        // echo "Urgency: " . $urgency . "<br>";
-                        // echo "Timeframe: " . $timeframe . "<br>";
-                        // echo "Sort Type: " . $sortType . "<br>";
-                        // echo "Sort Direction: " . $sortDirection . "<br>";
-
-                        
-                        // Filter feedback rows
-                        $feedbackRows = array_filter($feedbackRows, function($row) use ($Feedback_View, $resolved, $closed, $urgency, $timeframe) {
-                            return $Feedback_View->row_passes_filters($row, $resolved, $closed, $urgency, $timeframe);
-                        });
-
-                        
-
-                    } else {
-                        $feedbackRows = $Feedback_View->get_all_feedback(); // Default action, if not searching
-                    }
-
+            <!-- Table to show feedback -->
+            <table class="search-table">
 
 
                     
 
                     
-                    
-                    ?>
 
+            
+            </table>
+                   
 
-
-                    <div class="table" class="center">
-                        <table>
-
-                            <!-- Table Headers -->
-                            <tr>
-                                <th>Status</th> <!-- Resolved + Urgency + closed-->
-                                <th>Title</th> <!-- Title -->
-                                <th>Text</th> <!-- Text -->
-                                <th>Date</th> <!-- Date -->
-                                <th>Rating Points</th> <!-- RatingPoints -->
-                                <th>Comments</th> <!-- Number of comments -->
-                                <th>Course</th> <!-- Course -->
-                                <th>Author</th> <!-- Author -->
-                            </tr>
-
-                            <?php
-                            // Get user info for each feedback item
-                            foreach ($feedbackRows as $row) {
-                                $userInfo = $Feedback_View->get_user_info($row['feedbackID']);
-                            ?>
-
-                                <tr class="clickable-row" data-href="feedback.php">
-                                    <td>
-                                        <?php echo  $get_urgency_string[$row['urgency']] . "<br>" // Resolved Status - Refer to functions.php for the array
-                                                    . $get_resolved_string[$row['resolved']]     . "<br>" // Urgency Level   - Refer to functions.php for the array
-                                                    . $get_closed_string[$row['closed']];                 // Closed Status   - Refer to functions.php for the array
-                                        ?>
-                                        </td>
-                                        <td><?php echo shorten($row['title'], 50); ?></td> <!-- shortens the title to 50 characters -->
-                                        <td><?php echo shorten($row['text'], 75); ?></td> <!-- shortens the text to 15 characters -->
-                                        <td><?php echo $row['date']; ?></td> <!-- Date -->
-                                        <td><?php echo $row['ratingPoints']; ?></td> <!-- Rating Points -->
-                                        <td><?php echo $row['number_of_comments']; ?></td> <!-- Number of comments -->
-                                        <td><?php echo $userInfo['name'] ?></td>
-                                        <td href="profile.php">
-                                        <div style="display: flex;"> <!-- MAKE THIS GO TO THE THE CORECT PROFILE-->
-
-                                            <img style="margin-right: 10px;" class="avatar" src="<?php
-                                                                                                    // Get user info and find either jpg or png profile picture
-                                                                                                    $userID = $userInfo['userID'];
-                                                                                                    $jpg_path = "assets/profile-pictures/user-$userID.jpg";
-                                                                                                    $png_path = "assets/profile-pictures/user-$userID.png";
-
-                                                                                                    // Return the correct file path or default if neither found
-                                                                                                    if (file_exists($jpg_path)) {
-                                                                                                        echo $jpg_path;
-                                                                                                    } elseif (file_exists($png_path)) {
-                                                                                                        echo $png_path;
-                                                                                                    } else {
-                                                                                                        echo "assets/profile-pictures/user-default.jpg";
-                                                                                                    } ?>" alt="User Avatar" height="32" href="profile.php">
-
-
-                                            <a href="profile.php"> <?php echo $userInfo['username']; ?> </a> <!-- Username -->
-                                        </div>
-                                    </td>
-                                </tr>
-
-                            <?php
-                            }
-                            ?>
-
-                        </table>
-                    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    <script>
-                        const rows = document.querySelectorAll(".clickable-row");
-                        rows.forEach(row => {
-                            row.addEventListener("click", () => {
-                                window.location.href = row.dataset.href;
-                            });
-                        });
-                    </script>
-                </table>
+                
             </div>
         </div>
     </main>
+
+
     <!-- Footer -->
     <div class="footer-position"><?php include("footer.php"); ?></div>
-</body>
+    
 
-</html>
+
+
+<script>
+    const rows = document.querySelectorAll(".clickable-row");
+        rows.forEach(row => {
+            row.addEventListener("click", () => {
+                window.location.href = row.dataset.href;
+            });
+        });
+</script>
+
+
+<!-- import AJAX -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Updates table when changing filters, sorting, and searching -->
+<script>
+$(document).ready(function() {
+
+    // Function to update table
+    function updateTable() {
+
+            // Get filter values
+            var formData = $("#index-search-form").serialize();
+
+            // Send AJAX request
+            $.ajax({
+                type: "POST",
+                url: "updateTable.php", 
+                data: formData,
+                success: function(response) {
+                    // Update table with the response
+                    $(".search-table").html(response);
+                }
+            });
+
+
+        
+    }
+
+    // Listen for changes in filter inputs
+    $("#index-search-form input").on("change keyup", function() {
+        // Update table whenever filter inputs change
+        updateTable();
+    });
+
+    // Listen for changes in urgency slider
+    $("#urgency").on("input", function() {
+        // Update table whenever urgency slider changes
+        updateTable();
+    });
+
+    // Listen for changes in timeframe slider
+    $("#timeframe").on("input", function() {
+        // Update table whenever timeframe slider changes
+        updateTable();
+    });
+
+    // Listen for changes in resolved checkbox
+    $("input[name='resolved']").on("change", function() {
+        // Update table whenever resolved checkbox changes
+        updateTable();
+    });
+
+    // Listen for changes in closed checkbox
+    $("input[name='closed']").on("change", function() {
+        // Update table whenever closed checkbox changes
+        updateTable();
+    });
+
+    // Listen for changes in sort type dropdown
+    $("#sort-type").on("change", function() {
+        // Update table whenever sort type dropdown changes
+        updateTable();
+    });
+
+    // Listen for changes in sort direction dropdown
+    $("#Direction").on("change", function() {
+        // Update table whenever sort direction dropdown changes
+        updateTable();
+    });
+
+
+    // Initial table update
+    updateTable();
+
+});
+
+</script>
+
+
 
 
 
@@ -374,41 +296,21 @@ $Feedback_View = new FeedbackView($user_data['userID']);
             timeframeInput.value === '3' ? '1 Month' : 
             'All Time';
         });
+
+
+
+
+        
+
+
 </script>
 
 
 
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
-
-
-    $(document).ready(function() {
-        // Function to update table
-        function updateTable() {
-            // Get filter values
-            var formData = $("#index-search-form").serialize();
-
-            // Send AJAX request
-            $.ajax({
-                type: "POST",
-                url: "update_table.php", // Change this to the PHP file that handles the update
-                data: formData,
-                success: function(response) {
-                    // Update table with the response
-                    $(".table").html(response);
-                }
-            });
-        }
-
-        // Listen for changes in filter inputs
-        $("#index-search-form input, #index-search-form select").on("change keyup", function() {
-            // Update table whenever filter inputs change
-            updateTable();
-        });
-
-        // Initial table update
-        updateTable();
-    });
-</script> -->
 
 
 
+
+
+    </body>
+</html>
