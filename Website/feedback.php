2787bff3cb38467301cc76dc03d5fa27d25cfd1f
Toby Armond <27050587@students.lincoln.ac.uk>
2024-05-02 22:04:46 +0100

    got profile pictures working on comments

--- Website/feedback.php
+++ Website/feedback.php
@@ -30,6 +30,7 @@ $Feedback_Controller = new FeedbackContr($user_data['userID']);
 $feedback = $Feedback_Controller->feedback_get($feedbackID);
 
 $text = $feedback["text"];
+// FIXME does this need to be initialised with a value? it has red line underneath
 $comment_text;
 $user = $user_data['userID'];
 $feedback_userID = $feedback["userID"];
@@ -115,7 +116,20 @@ if (isset($_POST['submit_comment'])) {
             ?>
                 <div class="comment">
                     <div class="comment-header">
-                        <p><img class="avatar" src="assets/avatar.jpg" alt="User Avatar" height="32"><a href="profile.php"> <?= htmlspecialchars($comment_user_details['username'], ENT_QUOTES, 'UTF-8'); ?></a> commented <?= htmlspecialchars($comment['date'], ENT_QUOTES, 'UTF-8'); ?></p>
+                        <p><img class="avatar" src="<?php
+                            // Get user info and find either jpg or png profile picture
+                            $userID = $_SESSION['userID'];
+                            $jpg_path = "assets/profile-pictures/user-$comment_userID.jpg";
+                            $png_path = "assets/profile-pictures/user-$comment_userID.png";
+
+                            // Return
+                            if (file_exists($jpg_path)) {
+                                echo $jpg_path;
+                            } elseif (file_exists($png_path)) {
+                                echo $png_path;
+                            } else {
+                                echo "assets/profile-pictures/user-default.jpg";
+                            }?>" alt="User Avatar" height="32"><a href="profile.php"> <?= htmlspecialchars($comment_user_details['username'], ENT_QUOTES, 'UTF-8'); ?></a> commented <?= htmlspecialchars($comment['date'], ENT_QUOTES, 'UTF-8'); ?></p>
                     </div>
                     <div class="comment-main">
                         <p><?= htmlspecialchars($comment['text'], ENT_QUOTES, 'UTF-8'); ?></p>
