47062977dcc06e5d80fa1c2f747808ec33ade1eb
Archie Baldry <33762310+archiebaldry@users.noreply.github.com>
2024-05-03 16:01:34 +0100

    Remove broken dark/light mode scripts

--- Website/scripts/main.js
+++ Website/scripts/main.js
@@ -1,41 +1,3 @@
-function darkMode() {
-    // change the background color
-    let element = document.body;
-    element.className = "dark-mode";
-
-    // Change the button and icon
-    let button = document.getElementById("lightbulb-toggle");
-    let content = document.getElementById("lightbulb-symbol");
-    button.title = "Toggle Dark Mode";
-    button.onclick = lightMode; 
-    content.className = "fa-regular fa-lightbulb";
-    localStorage.setItem("theme", "dark-mode") // Save theme preference
-}
-
-function lightMode() {
-    // change the background color
-    let element = document.body;
-    element.className = "light-mode";
-
-    // Change the button and icon
-    let button = document.getElementById("lightbulb-toggle");
-    let content = document.getElementById("lightbulb-symbol");
-    button.title = "Toggle Light Mode";
-    button.onclick = darkMode; // Assign function reference
-    content.className = "fa-solid fa-lightbulb";
-    localStorage.setItem("theme", "light-mode") // Save theme preference
-}
-
-// Apply saved theme preference
-document.addEventListener("DOMContentLoaded", function() {
-    if (localStorage.getItem("theme") === "light-mode"){
-        lightMode();
-    }
-    else{
-        darkMode();
-    }
-});
-
 // Heart button functions
 function like() {
     let button = document.getElementById("heart-toggle");
@@ -93,17 +55,3 @@ function togglePasswordVisibility(passwordID) {
         element.type = "password";
     }
 }
-
-// Old Functions
-// function darkMode() {
-//     let element = document.body;
-//     let content = document.getElementById("DarkModetext");
-//     element.className = "dark-mode";
-//     content.innerText = "Dark Mode is ON";
-// }
-// function lightMode() {
-//     let element = document.body;
-//     let content = document.getElementById("DarkModetext");
-//     element.className = "light-mode";
-//     content.innerText = "Dark Mode is OFF";
-// }
